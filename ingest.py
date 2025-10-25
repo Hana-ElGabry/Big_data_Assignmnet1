d8931d48369ac726caabb2f8d725d064cc211ba4
Hana <h.hatem2262@nu.edu.eg>
2025-10-25 23:26:18 +0300

    modified ingest again

--- ingest.py
+++ ingest.py
@@ -1,14 +1,17 @@
 import pandas as pd
+import sys
 import os
 
 def main():
     print("\nSTEP 1: DATA INGESTION\n")
     
-    local_path = "Data/dirty_cafe_sales.csv"
-    
-    df = pd.read_csv(local_path)
+    file_path = sys.argv[1]
+    df = pd.read_csv(file_path)
     print(f"Loaded: {df.shape[0]} rows, {df.shape[1]} columns")
     print(f"\nMissing values:\n{df.isnull().sum()}\n")
+    
+    df.to_csv("data_raw.csv", index=False)
+    print("✓ Saved copy as: data_raw.csv\n")
     
     os.system("python preprocess.py")
 
